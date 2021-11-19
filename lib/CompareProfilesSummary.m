b8a4f0d15164c7f7533b80d96f460fcbe7bc34dc
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2021-11-19 10:01:59 +0100

    adding possibility to compare stats and summary data with cyProg as independent variable

--- lib/CompareProfilesSummary.m
+++ lib/CompareProfilesSummary.m
@@ -1,9 +1,13 @@
-function CompareProfilesSummary(profBARs,profFWHMs,profINTs,BARs,FWHMs,INTs)
+function CompareProfilesSummary(BARsProf,FWHMsProf,INTsProf,BARsSumm,FWHMsSumm,INTsSumm,cyProgsProf,cyProgsSumm)
+    fprintf("comparing stats based on profiles and stats based on summary files...\n");
+    if ( ~exist('cyProgsSumm','var') & exist('cyProgsProf','var') )
+        warning("...you gave me cyProgs from profiles but not those from the summary! Ignoring available info...");
+    end
     figure();
     nCols=3; % INTs, FWHMs, BARs
     nRows=2; % CAMeretta and DDS
-    nDataProf=size(profFWHMs,1);
-    nDataSumm=size(FWHMs,1);
+    nDataProf=size(FWHMsProf,1);
+    nDataSumm=size(FWHMsSumm,1);
     iPlot=0;
     for iRow=1:nRows
         switch iRow
@@ -16,21 +20,34 @@ function CompareProfilesSummary(profBARs,profFWHMs,profINTs,BARs,FWHMs,INTs)
             iPlot=iPlot+1;
             switch iCol
                 case 1
-                    whatProf=profFWHMs; whatSumm=FWHMs; myYLab="FWHM [mm]";
+                    whatProf=FWHMsProf; whatSumm=FWHMsSumm; myYLab="FWHM [mm]";
                 case 2
-                    whatProf=profBARs; whatSumm=BARs; myYLab="BAR [mm]";
+                    whatProf=BARsProf; whatSumm=BARsSumm; myYLab="BAR [mm]";
                 case 3
-                    whatProf=profINTs; whatSumm=INTs; myYLab="INT []";
+                    whatProf=INTsProf; whatSumm=INTsSumm; myYLab="INT []";
             end
             subplot(nRows,nCols,iPlot);
             for iPlane=1:2
                 if ( iPlane>1 ), hold on; end
-                plot([1:nDataProf],whatProf(:,iPlane,iRow),"o",...
-                     [1:nDataSumm],whatSumm(:,iPlane,iRow),"*");
+                if ( exist('cyProgsProf','var') & exist('cyProgsSumm','var') )
+                    xDataProf=str2double(cyProgsProf(:,iRow));
+                    xDataSumm=str2double(cyProgsSumm(:,iRow));
+                else
+                    xDataProf=1:nDataProf;
+                    xDataSumm=1:nDataSumm;
+                end
+                plot(xDataProf,whatProf(:,iPlane,iRow),"o",...
+                     xDataSumm,whatSumm(:,iPlane,iRow),"*");
             end
-            xlabel("ID []"); ylabel(myYLab); grid on; title(myTit);
+            if ( exist('cyProgsProf','var') & exist('cyProgsSumm','var') )
+                xlabel("cyProgs []");
+            else
+                xlabel("ID []");
+            end
+            ylabel(myYLab); grid on; title(myTit);
             if ( iCol==3 ), set(gca, 'YScale', 'log'); end
             legend("hor - prof","hor - summ","ver - prof","ver - summ","location","best");
         end
     end
+    fprintf("...end.\n");
 end
