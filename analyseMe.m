6fd77fcee9f67f755c9e215e24f787343a198025
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2021-11-19 11:53:40 +0100

    deploying plot for comparing currents

--- analyseMe.m
+++ analyseMe.m
@@ -27,24 +27,27 @@ run(sprintf("%s\\SetMeUp.m",dataTree));
 [Is,nDataCurr]=AcquireCurrentData(currPaths,LGENnames);
 % - parse LPOW monitor log
 [tStampsLPOWMon,LGENsLPOWMon,LPOWsLPOWMon,racksLPOWMon,repoValsLPOWMon,appValsLPOWMon,cyCodesLPOWMon,cyProgsLPOWMon,endCycsLPOWMon]=ParseLPOWLog(LPOWmonPaths);
+% - cyProgs of LPOWmon are off!!!!
+cyProgsLPOWMon=str2double(cyProgsLPOWMon)+4097; % 4097=2^12+1;
 return
 
 %% main - cross checks
 % - compare data from summary files and statistics computed on profiles
-CompareProfilesSummary(BARsProf,FWHMsProf,INTsProf,BARsSumm,FWHMsSumm,INTsSumm);
 CompareProfilesSummary(BARsProf,FWHMsProf,INTsProf,BARsSumm,FWHMsSumm,INTsSumm,cyProgsProf,cyProgsSumm);
+% - compare currents
+CompareCurrents(Is,indices,LGENnames,appValsLPOWMon,LGENsLPOWMon,allLGENs,cyProgsSumm,cyProgsLPOWMon); % indices are based on summary data
 % - plot distributions (3D visualisation)
 ShowParsedDistributions(profiles,Is,LGENnames,indices);
 return
 
 %% main - actual analysis
 % - raw plots
-% RawPlots(Is,nDataCurr,FWHMs,BARs,INTs,summNData,descs,outNames);
+% RawPlots(Is,nDataCurr,FWHMsSumm,BARsSumm,INTsSumm,nDataSumm,descs,outNames);
 RawPlots(Is,nDataCurr,FWHMsProf,BARsProf,INTsProf,nDataProf,descs,outNames);
 % - actual plots
 % ScanPlots(Is,FWHMsSumm,BARsSumm,indices,descs,outNames);
 ScanPlots(Is,FWHMsProf,BARsProf,indices,descs,outNames);
 % - export data to xlsx files
-% ExportData(Is,nDataCurr,FWHMs,BARs,INTs,summNData,indices,outNames);
+% ExportData(Is,nDataCurr,FWHMsSumm,BARsSumm,INTsSumm,nDataSumm,indices,outNames);
 ExportData(Is,nDataCurr,FWHMsProf,BARsProf,profINTs,nDataProf,indices,outNames);
 return
