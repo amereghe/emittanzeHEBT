0a7e3409f8bce29d152563ab8d607f95fd2fc3f5
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2021-11-25 10:46:15 +0100

    better structuring of SetMeUp and introduction of parameters for acquiring TM values

--- 2021-09/SetMeUp.m
+++ 2021-09/SetMeUp.m
@@ -1,11 +1,14 @@
+%% user input
 parentPath="2021\QuadScanSep2021";
 description="2021-09: RFKO";
+% subfolders in parentPath
 paths=[
     "ScanZ218-C270"
     ];
 currFiles=[
     "ScanZ218-RFKOC270mmSala1.xls"
     ];
+% scanning quads (1 per scan)
 LGENnames=[
     "PB-009A-LGEN"
     ];
@@ -28,9 +31,22 @@ LPOWlogFiles=[
     "2021-09-02\*.txt"
     "2021-09-03\*.txt"
     ];
+beamPart="CARBON";
+machine="LineZ";
+config="TM"; % select configuration: TM, RFKO
+
+% indices in measurements
+% - 1st col: CAM (summary file), DDS (summary file), current;
+% - 2nd col: min,max;
+indices=zeros(3,2,length(paths));
+indices(3,:,1)=[12 51];
+indices(1,:,1)=indices(3,:,1)-2;
+indices(2,:,1)=indices(3,:,1)-1;
+
+%% 
 fprintf("setting up data in %s - desc: %s ...\n",parentPath,description);
 
-% some consistency checks of user input
+%% some consistency checks of user input
 if ( length(paths)~=length(currFiles) ), error("...different number of paths (%d) and current files (%d)!",length(paths),length(currFiles)); end
 if ( length(paths)~=length(LGENnames) ), error("...different number of paths (%d) and LGEN names (%d)!",length(paths),length(LGENnames)); end
 if ( length(paths)~=length(LGENnickNames) ), error("...different number of paths (%d) and LGEN element names (%d)!",length(paths),length(LGENnickNames)); end
@@ -38,7 +54,7 @@ if ( length(paths)~=length(CAMpaths) ), error("...different number of paths (%d)
 if ( ~exist('DDSpaths','var') ), DDSpaths=strings(length(CAMpaths),1); DDSpaths(:)="PRC-544-*"; end
 if ( length(paths)~=length(DDSpaths) ), error("...different number of paths (%d) and paths to DDS data (%d)!",length(paths),length(DDSpaths)); end
 
-% build actual paths
+%% build actual paths
 currPaths=strings(length(paths),1);
 actCAMPaths=strings(length(paths),1);
 CAMProfsPaths=strings(length(paths),1);
@@ -58,11 +74,5 @@ for ii=1:length(LPOWlogFiles)
     LPOWmonPaths(ii)=sprintf("%s\\%s",LPOWmonPath,LPOWlogFiles(ii));
 end
 
-% indices in measurements
-indices=zeros(3,2,length(paths)); % 1st: CAM (summary file), DDS (summary file), current; 2nd: min,max;
-indices(3,:,1)=[12 51];
-indices(1,:,1)=indices(3,:,1)-2;
-indices(2,:,1)=indices(3,:,1)-1;
-
-%
+%%
 fprintf("...done\n");
