bd5afd80f896f13aac4221530cad7dfc6eae5eb6
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-01-19 17:17:43 +0100

    Prepaing function for acquiring LGEN data of scans from .xlsx files to be uplodaded to MatLabTools

--- lib/AcquireCurrent.m
+++
@@ -1,9 +0,0 @@
-function [Is,LGENnames]=AcquireCurrent(fileName,sheetName)
-    if ( ~exist('sheetName','var') ), sheetName="Foglio1"; end
-    fprintf("parsing current file %s, sheet %s ...\n",fileName,sheetName);
-    C=readcell(fileName,"Sheet",sheetName,"NumHeaderLines",1);
-    % storing data
-    Is=cell2mat(C(:,3:end));
-    LGENnames=strings(C(:,1));
-    fprintf("...done.\n");
-end
