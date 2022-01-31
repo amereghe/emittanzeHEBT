90eeec1255f30cf22b7d2bedb094b682842581bf
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-01-31 13:32:38 +0100

    preparing plotting functions of scans for being ported to MatLabTools

--- lib/RawPlots.m
+++ lib/RawPlots.m
@@ -1,18 +1,37 @@
-function RawPlots(Is,currNData,FWHMs,BARs,INTs,nData,scanDescription,actPlotName)
-    fprintf("plotting data (raw plots)...\n");
+function RawPlots(Is,FWHMs,BARs,INTs,nData,scanDescription,titleSeries,actPlotName)
+% RawPlots               to show FWxMs, BARs and BARicentres of scans 
+%                          as simple sequence of values; current values
+%                          used in the scan are shown as well.
+%
+% input:
+% - Is (float(:,1)): list of current values [A];
+% - FWHMs/BARs/INTs (float(nData,2,nSeries)): list of values of FWHM and
+%    BARicentre [mm], and integral of counts;
+%    the column indicates the plane:
+%    . 1: hor plane;
+%    . 2: ver plane;
+% - nData (float(nSeries,1)): number of rows in FWHMs/BARs/INTs for each
+%    series;
+% - scanDescription (string(1,1)): label used for the figure title;
+% - titleSeries (string(nSeries,1), optional): a single label for each
+%    series, e.g. indicating the name; if not given, the function
+%    associates specific names
+% - actPlotName (string(1,1), optional): label used to generate a .png file.
+%    Please check the variable MapFileOut in this function for the
+%    nomenclature. If not given, a window appears with the plot, and no
+%    .png file is saved;
+%    
+    fprintf("plotting raw data of scans: FWHMs, BARs and Integrals vs ID...\n");
     if ( exist('actPlotNames','var') )
         ff = figure('visible','off');
         ff.Position=[ ff.Position(1:2) 1.6*ff.Position(3:4) ]; % increase the default size of the plot
     else
         figure();
     end
-    for jj=1:2 % CAMeretta,DDS
-        switch jj
-            case 1
-                myMon="CAMeretta";
-            case 2
-                myMon="DDS";
-        end
+    if ( ~exist('titleSeries','var') || sum(ismissing(titleSeries)) )
+        titleSeries=compose("Series %02i",(1:size(FWHMs,3))');
+    end
+    for jj=1:size(FWHMs,3)
         for kk=1:3 % FWHM,BAR,INT
             switch kk
                 case 1
@@ -26,13 +45,13 @@ function RawPlots(Is,currNData,FWHMs,BARs,INTs,nData,scanDescription,actPlotName
             ax(iPlot)=subplot(3,3,iPlot);
             plot(whatToShow(1:nData(jj),1,jj),"*-"); hold on; plot(whatToShow(1:nData(jj),2,jj),"*-"); 
             grid on; ylabel(labelY); xlabel("ID []");
-            title(sprintf("%s - %s",whatName,myMon)); legend("HOR","VER","Location","best");
+            title(sprintf("%s - %s",whatName,titleSeries(jj))); legend("HOR","VER","Location","best");
         end
     end
     % corrente
     iPlot=iPlot+1;
     ax(iPlot)=subplot(3,3,iPlot);
-    plot(Is(1:currNData),"*-");
+    plot(Is,"*-");
     grid on; ylabel("[A]"); xlabel("ID []");
     title("Scan current");
     % general
--- lib/ScanPlots.m
+++ lib/ScanPlots.m
@@ -1,17 +1,35 @@
-function ScanPlots(Is,FWHMs,BARs,indices,scanDescription,actPlotName)
-    fprintf("plotting data (scan plots)...\n");
+function ScanPlots(Is,FWHMs,BARs,indices,scanDescription,titleSeries,actPlotName)
+% ScanPlots               to show FWxMs and BARs as a function of current
+%                          in single magnet scans
+%
+% input:
+% - Is (float(:,1)): list of current values [A];
+% - FWHMs/BARs (float(nData,2,nSeries)): list of values of FWHM and BARicentre [mm];
+%    the column indicates the plane:
+%    . 1: hor plane;
+%    . 2: ver plane;
+% - indices (float(1+nSeries,2)): min and max indices of the points
+%    of the actual scan in the series and in the currentsXLS array;
+%    NB: first index refers to currents in .xlsx file;
+% - scanDescription (string(1,1)): label used for the figure title;
+% - titleSeries (string(nSeries,1), optional): a single label for each
+%    series, e.g. indicating the name; if not given, the function
+%    associates specific names
+% - actPlotName (string(1,1), optional): label used to generate a .png file.
+%    Please check the variable MapFileOut in this function for the
+%    nomenclature. If not given, a window appears with the plot, and no
+%    .png file is saved;
+%    
+    fprintf("plotting scan data: FWxMs and BARs vs Iscan...\n");
     if ( exist('actPlotName','var') )
         ff = figure('visible','off');
     else
         figure();
     end
-    for jj=1:2 % CAMeretta,DDS
-        switch jj
-            case 1
-                myMon="CAMeretta";
-            case 2
-                myMon="DDS";
-        end
+    if ( ~exist('titleSeries','var') || sum(ismissing(titleSeries)) )
+        titleSeries=compose("Series %02i",(1:size(FWHMs,3))');
+    end
+    for jj=1:size(FWHMs,3)
         for kk=1:2 % FWHM,BAR
             switch kk
                 case 1
@@ -27,7 +45,7 @@ function ScanPlots(Is,FWHMs,BARs,indices,scanDescription,actPlotName)
             plot(Is(indices(1,1):indices(1,2)),whatToShow(indices(jj+1,1):indices(jj+1,2),2,jj),"*-"); ylabel(sprintf("VER %s",labelY));
             yyaxis left;
             grid on; xlabel("I [A]");
-            title(sprintf("%s - %s",whatName,myMon)); % legend("HOR","VER","Location","best");
+            title(sprintf("%s - %s",whatName,titleSeries(jj))); % legend("HOR","VER","Location","best");
         end
     end
     % general
