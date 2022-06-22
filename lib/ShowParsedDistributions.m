9dcf0bfa59b12974a42aabc4f077583c0e291f38
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-06-22 15:01:23 +0200

    bug fixes in ShowParsedDistributions.m: * reordered interface vars + checked actual optional arguments; * added saving of .fig picture;

--- lib/ShowParsedDistributions.m
+++ lib/ShowParsedDistributions.m
@@ -1,9 +1,11 @@
-function ShowParsedDistributions(profiles,Is,LGENscanned,indices)
+function ShowParsedDistributions(profiles,LGENscanned,actPlotName,Is,indices)
     fprintf("plotting distributions...\n");
-    if ( exist('indices','var') )
+    if ( exist('Is','var') & exist('indices','var') )
         myRemark="aligned distributions";
-    else
+    elseif ( ~exist('Is','var') & ~exist('indices','var') )
         myRemark="all distributions";
+    else
+        error("please provide me with both Is and indices!");
     end
     monitorNames=[ "CAMeretta" "DDS" ];
     planeNames=[ "Hor plane" "Ver plane" ];
@@ -14,8 +16,8 @@ function ShowParsedDistributions(profiles,Is,LGENscanned,indices)
         for iPlane=1:2
             iPlot=iPlot+1;
             subplot(2,2,iPlot);
-            if ( exist('indices','var') )
-                PlotSpectra(profiles(:,[1 indices(iMon,1):indices(iMon,2) ],iPlane,iMon),false,Is(indices(3,1):indices(3,2)),"I [A]");
+            if ( exist('Is','var') )
+                PlotSpectra(profiles(:,[1 indices(iMon,1):indices(iMon,2) ],iPlane,iMon),false,Is(indices(1,1):indices(1,2)),"I [A]");
             else
                 PlotSpectra(profiles(:,:,iPlane,iMon));
             end
@@ -24,5 +26,19 @@ function ShowParsedDistributions(profiles,Is,LGENscanned,indices)
         end
     end
     sgtitle(sprintf("%s - %s",myRemark,LabelMe(LGENscanned)));
+    
+    % save figure
+    if ( exist('actPlotName','var') )
+        if ( strlength(actPlotName)>0 )
+            if ( exist('Is','var') )
+                MapFileOut=sprintf("%s_3D_reduced.fig",actPlotName);
+            else
+                MapFileOut=sprintf("%s_3D.fig",actPlotName);
+            end
+            savefig(MapFileOut);
+            fprintf("...saving to file %s ...\n",MapFileOut);
+        end
+    end
+    
     fprintf("...done.\n");
 end
