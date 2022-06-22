83b3d34c8215040d9864c42bba600172151cddc6
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-06-22 14:59:44 +0200

    bug fixes to CompareCurrents.m: * CyProgs are strings; * legends are not overwritten;

--- lib/CompareCurrents.m
+++ lib/CompareCurrents.m
@@ -7,7 +7,7 @@ function CompareCurrents(Is,indices,LGENscanned,appValsLPOWMon,LGENsLPOWMon,allL
     % align currents from excel to those from LPOW mon based on cyProgs
     % NB: we are using cyProgs from beam data (use CAMeretta data)!
     xlsCurrs=Is(indices(1,1):indices(1,2));
-    measCyProgs=str2double(cyProgsMeas(indices(2,1):indices(2,2)));
+    measCyProgs=cyProgsMeas(indices(2,1):indices(2,2));
 
     % selected LGEN
     subplot(1,2,1);
@@ -15,9 +15,9 @@ function CompareCurrents(Is,indices,LGENscanned,appValsLPOWMon,LGENsLPOWMon,allL
     tmpCyProgs=cyProgsLPOWMon(tmpIndices);
     tmpAppliedVals=appValsLPOWMon(tmpIndices);
     [vals,ia,ib]=intersect(measCyProgs,tmpCyProgs);
-    plot(measCyProgs(ia),xlsCurrs(ia),"k*"); hold on;
+    plot(str2double(measCyProgs(ia)),xlsCurrs(ia),"k*"); hold on;
     ii=find(strcmpi(allLGENs,LGENscanned)); % ii is used to select the correct color in the color map
-    plot(tmpCyProgs(ib),tmpAppliedVals(ib),".-","Color",cm(end-(ii-1),:));
+    plot(str2double(tmpCyProgs(ib)),tmpAppliedVals(ib),".-","Color",cm(end-(ii-1),:));
     grid on; xlabel("cyProgs []"); ylabel("I [A]"); title(LabelMe(LGENscanned));
     legend("LPOW log","xls","Location","best");
 
@@ -34,14 +34,14 @@ function CompareCurrents(Is,indices,LGENscanned,appValsLPOWMon,LGENsLPOWMon,allL
         if ( length(vals)==0 )
             warning("...no data available in LPOWmon log for %s - going with TM values",allLGENs(ii));
             Xs=measCyProgs;
-            Ys=scannedIsTM(:,ii);
+            Ys=scannedIsTM(indices(1,1):indices(1,2),ii);
             myLeg=[myLeg sprintf("%s - TM",allLGENs(ii))];
         else
             Xs=tmpCyProgs(ib);
             Ys=tmpAppliedVals(ib);
+            myLeg=[myLeg allLGENs(ii)];
         end
-        plot(Xs,Ys,".-","Color",cm(end-(ii-1),:));
-        myLeg=[myLeg allLGENs(ii)];
+        plot(str2double(Xs),Ys,".-","Color",cm(end-(ii-1),:));
     end
     grid on; xlabel("cyProgs []"); ylabel("I [A]"); title("All other LGEN of interest");
     legend(myLeg,"Location","best");
