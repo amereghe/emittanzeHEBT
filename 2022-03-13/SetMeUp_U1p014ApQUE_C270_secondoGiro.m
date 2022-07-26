050df2a000f28b97804d5eadf946f35566837813
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-07-26 14:20:42 +0200

    updating Setting up scripts

--- 2022-03-13/SetMeUp_U1p014ApQUE_C270_secondoGiro.m
+++ 2022-03-13/SetMeUp_U1p014ApQUE_C270_secondoGiro.m
@@ -1,12 +1,12 @@
 %% user input
-description="2022-03: U1-008A-QUE";      % just a label
+description="2022-03: U1-014A-QUE";      % just a label
 parentPath="2022\Emittanze 2022";
-path="secondo giro\Scan U1-08-H23";      % subfolder in parentPath
+path="secondo giro\Scan U1-14-H24";      % subfolder in parentPath
 currFile="Scan01QuadU.xlsx";
-LGENscanned="P9-003A-LGEN";              % scanning quad
-LGENscannedNickName="U1-008A-QUE";
-scanDescription="scan: U1-008A-QUE - C, 270 mm - secondo Giro";
-plotName="U1-008A-QUE_C270_secondoGiro";
+LGENscanned="P9-004A-LGEN";              % scanning quad
+LGENscannedNickName="U1-014A-QUE";
+scanDescription="scan: U1-014A-QUE - C, 270 mm - secondo Giro";
+plotName="U1-014A-QUE_C270_secondoGiro";
 CAMpaths=[
     "CarbSO1_LineU_Size6_*"
     ];
@@ -28,7 +28,7 @@ indices(2,:)=indices(1,:)-2;
 indices(3,:)=indices(1,:)-1;
 % indices for fitting data
 fitIndices=zeros(3,2,7);
-fitIndices(2,:,:)=[8 26; 9 25; 10 24; 11 23; 12 22; 13 21; 14 20]'; % symmetric, 7 couples
+fitIndices(2,:,:)=[11 30; 12 29; 13 28; 14 27; 15 26; 16 25; 17 24]'; % symmetric, 7 couples
 % fitIndices(2,:,:)=[7 23; 7 24; 7 25; 7 26; 8 23; 8 24; 8 25; 8 26; 9 23; 9 24; 9 25; 9 26; 10 23; 10 24; 10 25; 10 26; ]'; % asymmetric, 16 couples
 fitIndices(1,:,:)=fitIndices(2,:,:)+2;
 fitIndices(3,:,:)=fitIndices(2,:,:)+1;
