7deea58742d5ba1e0a049be2bde64f7273ec1531
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-07-26 14:23:44 +0200

    adding a couple of scripts showing results of fits

---
+++ lib/ShowFittedEllipsesGrouped.m
@@ -0,0 +1,40 @@
+function ShowFittedEllipsesGrouped(beta,alpha,emiG,planeLabs,labels1,labels2,labels3,myTit)
+    nFitSets=size(beta,2); nSig=1;
+    nSeries=size(beta,4);
+    [nRows,nCols]=GetNrowsNcols(nSeries);
+    if ( nRows*nCols==nSeries ), nRows=nRows+1; end
+    
+    %% actually plot
+    for iLab3=1:length(labels3)
+        figure();
+        cm=colormap(parula(nFitSets));
+        ii=0;
+        for iPar2=1:length(labels1)
+            for iPlane=1:length(planeLabs)
+                ii=ii+1; axs(iPar2,iPlane)=subplot(nRows,2*nCols,ii);
+                for iFitSet=1:nFitSets
+                    mySig=sqrt(beta(iLab3,iFitSet,iPlane,iPar2)*emiG(iLab3,iFitSet,iPlane,iPar2));
+                    xx=linspace(-nSig*mySig,nSig*mySig,100);
+                    yp=(-alpha(iLab3,iFitSet,iPlane,iPar2)*xx+sqrt(mySig^2-xx.^2))/beta(iLab3,iFitSet,iPlane,iPar2);
+                    yn=(-alpha(iLab3,iFitSet,iPlane,iPar2)*xx-sqrt(mySig^2-xx.^2))/beta(iLab3,iFitSet,iPlane,iPar2);
+                    if ( iFitSet>1 ), hold on; end
+                    plot([xx xx(end:-1:1)]*1E3,[yp yn(end:-1:1)]*1E3,".-","Color",cm(iFitSet,:));
+                end
+                title(sprintf("%s plane - %s",planeLabs(iPlane),labels1(iPar2))); xlabel("z [mm]"); ylabel("zp [mrad]"); grid();
+            end
+        end
+        % dedicated plot for the legend
+        ii=ii+1; subplot(nRows,2*nCols,ii);
+        for iFitSet=1:nFitSets
+            if ( iFitSet>1 ), hold on; end
+            plot(NaN(),NaN(),".-","Color",cm(iFitSet,:));
+        end
+        legend(labels2,"Location","best");
+        % general
+        sgtitle(sprintf("%s - %s",labels3(iLab3),myTit));
+        if ( length(labels1)>1 )
+            linkaxes(axs(:,1),"xy"); % all HOR ellypses
+            linkaxes(axs(:,2),"xy"); % all VER ellypses
+        end
+    end
+end
---
+++ lib/ShowFittedOpticsFunctionsGrouped.m
@@ -0,0 +1,79 @@
+function ShowFittedOpticsFunctionsGrouped(beta,alpha,emiG,disp,dispP,zz,zp,xVals,xLab,planeLabs,labels1,labels2,myTit)
+
+    nFitSets=size(beta,4);
+    
+    for iPlane=1:length(planeLabs)
+        for iLab2=1:length(labels2)
+            figure();
+            cm=colormap(parula(nFitSets));
+            ii=0;
+            %% first row
+            % - beta
+            ii=ii+1; ax(ii)=subplot(3,3,ii);
+            for iFitSet=1:nFitSets
+                if ( iFitSet>1 ), hold on; end
+                plot(xVals,beta(iLab2,:,iPlane,iFitSet),".-","Color",cm(iFitSet,:));
+            end
+            title("\beta"); ylabel("[m]"); grid(); xlabel(xLab);
+            % - alpha
+            ii=ii+1; ax(ii)=subplot(3,3,ii);
+            for iFitSet=1:nFitSets
+                if ( iFitSet>1 ), hold on; end
+                plot(xVals,alpha(iLab2,:,iPlane,iFitSet),".-","Color",cm(iFitSet,:));
+            end
+            title("\alpha"); ylabel("[]"); grid(); xlabel(xLab);
+            % - geometric emittance
+            ii=ii+1; ax(ii)=subplot(3,3,ii);
+            for iFitSet=1:nFitSets
+                if ( iFitSet>1 ), hold on; end
+                plot(xVals,emiG(iLab2,:,iPlane,iFitSet)*1E6,".-","Color",cm(iFitSet,:));
+            end
+            title("\epsilon"); ylabel("[\mum]"); grid(); xlabel(xLab);
+            %% second row
+            % - disp
+            ii=ii+1; ax(ii)=subplot(3,3,ii);
+            for iFitSet=1:nFitSets
+                if ( iFitSet>1 ), hold on; end
+                plot(xVals,disp(iLab2,:,iPlane,iFitSet),".-","Color",cm(iFitSet,:));
+            end
+            title("D"); ylabel("[m]"); grid(); xlabel(xLab);
+            % - disp prime
+            ii=ii+1; ax(ii)=subplot(3,3,ii);
+            for iFitSet=1:nFitSets
+                if ( iFitSet>1 ), hold on; end
+                plot(xVals,dispP(iLab2,:,iPlane,iFitSet),".-","Color",cm(iFitSet,:));
+            end
+            title("D'"); ylabel("[]"); grid(); xlabel(xLab); 
+            % - legend
+            ii=ii+1; ax(ii)=subplot(3,3,ii);
+            for iFitSet=1:nFitSets
+                if ( iFitSet>1 ), hold on; end
+                plot(NaN(),NaN(),".-","Color",cm(iFitSet,:));
+            end
+            legend(labels1,"Location","best");
+            %% third row
+            % - orbit
+            ii=ii+1; ax(ii)=subplot(3,3,ii);
+            for iFitSet=1:nFitSets
+                if ( iFitSet>1 ), hold on; end
+                plot(xVals,zz(iLab2,:,iPlane,iFitSet)*1E3,".-","Color",cm(iFitSet,:));
+            end
+            title("z"); ylabel("[mm]"); grid(); xlabel(xLab);
+            % - orbit prime
+            ii=ii+1; ax(ii)=subplot(3,3,ii);
+            for iFitSet=1:nFitSets
+                if ( iFitSet>1 ), hold on; end
+                plot(xVals,zp(iLab2,:,iPlane,iFitSet)*1E3,".-","Color",cm(iFitSet,:));
+            end
+            title("pz"); ylabel("[mrad]"); grid(); xlabel(xLab);
+
+            %% general
+            linkaxes(ax,"x");
+            if ( length(labels2)>1 )
+                sgtitle(sprintf("%s plane - %s - %s",planeLabs(iPlane),labels2(iLab2),myTit));
+            else
+                sgtitle(sprintf("%s plane - %s",planeLabs(iPlane),myTit));
+            end
+        end
+    end
+end
